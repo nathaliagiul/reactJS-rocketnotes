c2a56e79148ba2417a3f66787f140b075dabfd6e
Nathalia Alves <nathalia.giulliane@outlook.com>
2024-11-13 10:14:16 -0300

    Estrutura da Nova página

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Details } from './pages/Details'
+import { Home } from './pages/Home'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
+
+/* 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Details />
+    </ThemeProvider>
+  </StrictMode>,
+)
+
+*/
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode >
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Home />
     </ThemeProvider>
   </StrictMode>,
 )
