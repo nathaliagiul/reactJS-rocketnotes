57e7a980263bb0e0c06d572d66471e9aa609e4bd
Nathalia Alves <nathalia.giulliane@outlook.com>
2024-11-13 13:33:05 -0300

    New Note

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
-import { Profile } from './pages/Profile'
+import { New } from './pages/New'
 
 /* 
 
@@ -42,6 +42,16 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>
 
 )
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode >
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Profile />
+    </ThemeProvider>
+  </StrictMode>
+
+)
 */
 
 
@@ -49,7 +59,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Profile />
+      <New />
     </ThemeProvider>
   </StrictMode>
 
