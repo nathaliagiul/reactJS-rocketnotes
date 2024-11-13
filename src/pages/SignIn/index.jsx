064b3f799c37cf58a989c238489d3718f092b9df
Nathalia Alves <nathalia.giulliane@outlook.com>
2024-11-13 12:04:05 -0300

    Background SignIn

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,7 +1,7 @@
 import { FiMail, FiLock } from 'react-icons/fi'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
-import { Container, Form } from './styles'
+import { Container, Form, Background } from './styles'
 
 export function SignIn() {
   return (
@@ -18,7 +18,7 @@ export function SignIn() {
 
         <a href="#">Criar conta</a>
       </Form>
-      
+    <Background />
     </Container>
   )
 }
