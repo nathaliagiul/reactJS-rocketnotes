05c4f6cb9c22f06d7c2d00b4bf11e3e757cf481e
Nathalia Alves <nathalia.giulliane@outlook.com>
2024-11-13 11:06:19 -0300

    Notes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,7 @@
 import { FiPlus, FiSearch } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
+import { Note } from '../../components/Note'
+import { Section } from '../../components/Section'
 import { Header } from '../../components/Header'
 import { ButtonText } from '../../components/ButtonText'
 import { Input } from '../../components/Input'
@@ -24,7 +26,18 @@ export function Home() {
         <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
       </Search>
 
-      <Content></Content>
+      <Content>
+        <Section title="Minhas notas">
+            <Note data={{
+              title: 'React',
+              tags: [
+                { id: '1', name: 'react' },
+                { id: '2', name: 'rocketseat' }
+              ]
+            }}
+            />
+          </Section>
+      </Content>
 
       <NewNote>
         <FiPlus />
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -47,7 +47,8 @@ export const Search = styled.div`
 
 export const Content = styled.div`
   grid-area: content;
-  background-color: blue;
+  padding: 0 64px;
+  overflow-y: auto;
 `
 
 export const NewNote = styled.button`
